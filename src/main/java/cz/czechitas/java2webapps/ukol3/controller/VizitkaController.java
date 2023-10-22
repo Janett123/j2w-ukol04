ebf7773ad2121cc3f9e10eb2ca3edaf33451408b
Janett123 <masiko@centrum.cz>
2023-10-22 12:38:47 +0200

    Vytvoření nové vizitky

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -5,6 +5,7 @@ import cz.czechitas.java2webapps.ukol3.service.VizitkaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -35,4 +36,15 @@ public class VizitkaController {
     result.addObject("vizitka", service.getById(id));
     return result;
   }
+
+  @GetMapping ("/nova")
+  public String nova() {
+    return "nova";
+  }
+
+@PostMapping ("/")
+  public String append (Vizitka vizitka) {
+    service.append(vizitka);
+    return "redirect:/";
+}
 }
